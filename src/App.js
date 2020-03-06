102c92c312248b06d220c25c36bdbd8de8edf95e
sweeney153 <seansweeney153@gmail.com>
2020-03-06 16:50:07 -0500

    functional links in nav bar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Search from './components/users/Search';
 import Users from './components/users/Users';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
 	state = {
 		users: [],
-		loading: false
+		loading: false,
+		alert: null
 	};
-
-	// async componentDidMount() {
-	// 	this.setState({ loading: true });
-
-	// 	const res = await axios.get(
-	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
-	// 			.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-	// 	);
-
-	// 	this.setState({ users: res.data, loading: false });
-	// }
 
 	// Searching gitusers
 	searchUsers = async (text) => {
+		this.setState({ loading: true });
+
 		const res = await axios.get(
 			`https://api.github.com/search/users?q=${text}&client_id=${process.env
 				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
@@ -32,15 +27,46 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	// Clear users
+	clearUsers = () => {
+		this.setState({ users: [], loading: false });
+	};
+
+	// Setting alert
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+		setTimeout(() => this.setState({ alert: null }), 5000);
+	};
+
 	render() {
+		const { users, loading } = this.state;
 		return (
-			<div className="App">
-				<Navbar title="Github Finder" icon="fab fa-github" />
-				<div className="container">
-					<Search searchUsers={this.searchUsers} />
-					<Users loading={this.state.loading} users={this.state.users} />
+			<Router>
+				<div className="App">
+					<Navbar title="Github Finder" icon="fab fa-github" />
+					<div className="container">
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path="/"
+								render={(props) => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={users.length > 0 ? true : false}
+											setAlert={this.setAlert}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+							<Route exact path="/about" component={About} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
 	state = {
 		text: ''
 	};
 
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired
+	};
+
 	onSubmit = (e) => {
 		e.preventDefault();
-		this.props.searchUsers(this.state.text);
+		if (this.state.text === '') {
+			this.props.setAlert('Please enter something', 'light');
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: '' });
+		}
 	};
 
 	onChange = (e) => {
@@ -15,6 +28,8 @@ export class Search extends Component {
 	};
 
 	render() {
+		const { showClear, clearUsers } = this.props;
+
 		return (
 			<div>
 				<form onSubmit={this.onSubmit} className="form">
@@ -27,6 +42,11 @@ export class Search extends Component {
 					/>
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
+				{showClear && (
+					<button className="btn btn-light btn-block" onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
